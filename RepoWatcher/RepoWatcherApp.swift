c0064e619c6e0716c1a2653268ff211e025560a3
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-23 20:30:25 -0500

    Initial Commit

---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Danielle Lewis on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
