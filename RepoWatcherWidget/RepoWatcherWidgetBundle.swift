bca5863282823d180eb63d87805b508ab31cad9f
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-23 20:32:34 -0500

    - add widget target to app

---
+++ RepoWatcherWidget/RepoWatcherWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RepoWatcherWidgetBundle.swift
+//  RepoWatcherWidget
+//
+//  Created by Danielle Lewis on 12/23/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RepoWatcherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RepoWatcherWidget()
+    }
+}
