2cf7d4f6a9ee7d5bff4ba67385b81d2bb8a9b533
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-01-03 21:05:25 -0500

    - build UI for main app functionality

--- RepoWatcher/ContentView.swift
+++ RepoWatcher/ContentView.swift
@@ -4,20 +4,54 @@
 //
 //  Created by Danielle Lewis on 12/23/23.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newRepo = ""
+    @State private var repos: [String] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    TextField("Ex. dlewisdev/CoinInfo", text: $newRepo)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                        .textFieldStyle(.roundedBorder)
+
+                    Button {
+
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.green)
+                    }
+                }
+                .padding()
+
+                VStack(alignment: .leading) {
+                    Text("Saved Repos")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                        .padding(.leading)
+
+                    List(repos, id: \.self) { repo in
+                        Text(repo)
+                            .swipeActions {
+                                Button("Delete") {
+
+                                }
+                                .tint(.red)
+                            }
+                    }
+                }
+            }
+            .navigationTitle("Repo List")
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
